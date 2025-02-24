f36842c2ccf834e5096f3fcc167bb130169374f7
Ton Machielsen--replace-all <ton@machielsen.net>
2025-02-24 09:22:14 +0100

    New start based on VM's.

--- scripts/pg1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo -i cp /data.sql /var/lib/edb-as
-sudo -i chown enterprisedb:enterprisedb /var/lib/edb-as/data.sql
-sudo -i chmod 775 /var/lib/edb-as/data.sql
-sudo -i -u enterprisedb psql -d edb -f /var/lib/edb-as/data.sql
